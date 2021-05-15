735300ce558269311abec5e6069e14c474450403
Radu Moraru <randreimoraru@gmail.com>
2021-05-15 13:14:43 +0300

    Select + Delete

---
+++ src/queue/queue.h
@@ -0,0 +1,8 @@
+//
+// Created by radua on 5/14/2021.
+//
+
+#ifndef USOSQL_QUEUE_H
+#define USOSQL_QUEUE_H
+
+#endif //USOSQL_QUEUE_H
---
+++ src/trie/trie.h
@@ -0,0 +1,8 @@
+//
+// Created by radua on 5/14/2021.
+//
+
+#ifndef USOSQL_TRIE_H
+#define USOSQL_TRIE_H
+
+#endif //USOSQL_TRIE_H
